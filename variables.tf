4760af40868f83927537c493946476564df5c330
UshaAngular <167160481+UshaAngular@users.noreply.github.com>
2025-06-30 14:42:33 +1200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "inbound_bucket_name" {
+  description = "Name for the inbound S3 bucket"
+  type        = string
+}
+
+variable "outbound_bucket_name" {
+  description = "Name for the outbound S3 bucket"
+  type        = string
+}
